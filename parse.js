aa1ffd6abdc7b37ebfc9bb0d5a0c0ecd0ff97097
nanopick <shestakova163@gmail.com>
2022-10-16 12:04:13 -0400

    First commit

---
+++ parse.js
@@ -0,0 +1,13 @@
+
+<script>
+// Create a Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
+
+document.getElementById("demo").innerHTML = fruits.get("apples");
+</script>
+
